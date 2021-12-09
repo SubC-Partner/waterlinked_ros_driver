6cb48cd84f6a24760857c9dc1dc3e094ea8be1c2
Fredrik Soerensen <ffso@et.aau.dk>
2021-12-09 17:08:07 +0100

    Fixed subscriber placement

--- scripts/waterlinked_sbl_node.py
+++ scripts/waterlinked_sbl_node.py
@@ -48,7 +48,7 @@ def waterlinked(base_url):
                         PointStamped, queue_size=120)
     global_pub = rospy.Publisher('global_position',
                                   NavSatFix, queue_size=120)
-
+    
     def set_depth(data,args):
         g = 9.80665 #Gravitational acceleration
         rho = 997.0474 #Fresh Water, Salt Water = 1023.6
@@ -56,7 +56,10 @@ def waterlinked(base_url):
         rospy.loginfo("Depth: %s",depth)
 	payload = dict(depth=depth, temp=6.5) #Depth Fixed temp for now (degC).
         r = requests.put("{}/api/v1/external/depth".format(base_url), json=payload, timeout=10)
-        return r.json()
+
+    #Skal rettes til i forhold til publisher fra depth sensor.    
+    rospy.Subscriber("pressure",Float32,set_depth,base_url)
+
 
     def publish_raw(data_raw):
         raw_point = PointStamped()
@@ -77,10 +80,8 @@ def waterlinked(base_url):
         filtered_pub.publish(filtered_point)
 
     last_raw, last_filtered = None, None
-    while not rospy.is_shutdown():
-        #Skal rettes til i forhold til publisher fra depth sensor.
-        rospy.Subscriber("pressure",Float32,set_depth,base_url)
 
+    while not rospy.is_shutdown():        
         data_raw = get_acoustic_position_raw(base_url)
 
         if last_raw is None:
